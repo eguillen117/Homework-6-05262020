5a807cd0d907ef2f4cf449b88adf4d18b3f7fcdb
Eduardo Guillen <eguillen117@gmail.com>
2020-05-22 15:59:42 -0500

    Added functions in Javascript

--- javascript.js
+++ javascript.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
 	$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 	//target tbody to manipulate the table rows ... and more
 	var tbody = $('#calendar-content');
+
+	$('#cityForm').on('submit', function(event) {
+		//Code below prevents the page reloading
+		event.preventDefault();
+		var searchTerm = $('#search-term').val().trim();
+		console.log(searchTerm);
+		gettingJSON(searchTerm);
+	});
 });
 // END OF current day function to get present date=============================================================
 /**
@@ -16,149 +24,158 @@ $(document).ready(function() {
 	 * @returns {string} URL for NYT API based on form inputs
 	 */
 
-function gettingJSON() {
-	document.write('jquery loaded');
+function gettingJSON(searchTerm) {
 	$.getJSON(
-		'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=ee6596241130f193adf1ba90e625cc10',
+		'http://api.openweathermap.org/data/2.5/weather?q=' +
+			searchTerm +
+			'&units=imperial&APPID=46e6f2e7796e9e6d3d3f2cc4d3f59ec2',
 		function(json) {
-			document.write(JSON.stringify(json));
+			// document.write(JSON.stringify(json));
+			console.log(json);
+			$('#temperature').text(json.main.temp);
 		}
 	);
 }
-function buildQueryURL() {
-	// queryURL is the url we'll use to query the API
-	var queryURL = 'pro.openweathermap.org/data/2.5/forecast/hourly?id={London}&appid=46e6f2e7796e9e6d3d3f2cc4d3f59ec2';
 
-	// Begin building an object to contain our API call's query parameters
-	// Set the API key
-	var queryParams = { 'api-key': 'R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M' };
+//CREATE A NEW FUNCTION FOR THE 5 DAY. SEPERATE URL
+//OPEN WEATHER API ON THE DOCUMENTATION THEY HAVE ANOTHER ONE FOR FORECAST. 
 
-	// Grab text the user typed into the search input, add to the queryParams object
-	queryParams.q = $('#search-term').val().trim();
 
-	// If the user provides a startYear, include it in the queryParams object
-	var startYear = $('#start-year').val().trim();
 
-	if (parseInt(startYear)) {
-		queryParams.begin_date = startYear + '0101';
-	}
+// function buildQueryURL() {
+// 	// queryURL is the url we'll use to query the API
+// 	var queryURL = 'pro.openweathermap.org/data/2.5/forecast/hourly?id={London}&appid=46e6f2e7796e9e6d3d3f2cc4d3f59ec2';
 
-	// If the user provides an endYear, include it in the queryParams object
-	var endYear = $('#end-year').val().trim();
+// 	// Begin building an object to contain our API call's query parameters
+// 	// Set the API key
+// 	var queryParams = { 'api-key': 'R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M' };
 
-	if (parseInt(endYear)) {
-		queryParams.end_date = endYear + '0101';
-	}
+// 	// Grab text the user typed into the search input, add to the queryParams object
+// 	queryParams.q = $('#search-term').val().trim();
 
-	// Logging the URL so we have access to it for troubleshooting
-	console.log('---------------\nURL: ' + queryURL + '\n---------------');
-	console.log(queryURL + $.param(queryParams));
-	return queryURL + $.param(queryParams);
-}
+// 	// If the user provides a startYear, include it in the queryParams object
+// 	var startYear = $('#start-year').val().trim();
 
-/**
-	 * takes API data (JSON/object) and turns it into elements on the page
-	 * @param {object} NYTData - object containing NYT API data
-	 */
-function updatePage(NYTData) {
-	// Get from the form the number of results to display
-	// API doesn't have a "limit" parameter, so we have to do this ourselves
-	var numArticles = $('#article-count').val();
+// 	if (parseInt(startYear)) {
+// 		queryParams.begin_date = startYear + '0101';
+// 	}
 
-	// Log the NYTData to console, where it will show up as an object
-	console.log(NYTData);
-	console.log('------------------------------------');
+// 	// If the user provides an endYear, include it in the queryParams object
+// 	var endYear = $('#end-year').val().trim();
 
-	// Loop through and build elements for the defined number of articles
-	for (var i = 0; i < numArticles; i++) {
-		// Get specific article info for current index
-		var article = NYTData.response.docs[i];
+// 	if (parseInt(endYear)) {
+// 		queryParams.end_date = endYear + '0101';
+// 	}
 
-		// Increase the articleCount (track article # - starting at 1)
-		var articleCount = i + 1;
+// 	// Logging the URL so we have access to it for troubleshooting
+// 	console.log('---------------\nURL: ' + queryURL + '\n---------------');
+// 	console.log(queryURL + $.param(queryParams));
+// 	return queryURL + $.param(queryParams);
+// }
 
-		// Create the  list group to contain the articles and add the article content for each
-		var $articleList = $('<ul>');
-		$articleList.addClass('list-group');
+// /**
+// 	 * takes API data (JSON/object) and turns it into elements on the page
+// 	 * @param {object} NYTData - object containing NYT API data
+// 	 */
+// function updatePage(NYTData) {
+// 	// Get from the form the number of results to display
+// 	// API doesn't have a "limit" parameter, so we have to do this ourselves
+// 	var numArticles = $('#article-count').val();
 
-		// Add the newly created element to the DOM
-		$('#article-section').append($articleList);
+// 	// Log the NYTData to console, where it will show up as an object
+// 	console.log(NYTData);
+// 	console.log('------------------------------------');
 
-		// If the article has a headline, log and append to $articleList
-		var headline = article.headline;
-		var $articleListItem = $("<li class='list-group-item articleHeadline'>");
+// 	// Loop through and build elements for the defined number of articles
+// 	for (var i = 0; i < numArticles; i++) {
+// 		// Get specific article info for current index
+// 		var article = NYTData.response.docs[i];
 
-		if (headline && headline.main) {
-			console.log(headline.main);
-			$articleListItem.append(
-				"<span class='label label-primary'>" +
-					articleCount +
-					'</span>' +
-					'<strong> ' +
-					headline.main +
-					'</strong>'
-			);
-		}
+// 		// Increase the articleCount (track article # - starting at 1)
+// 		var articleCount = i + 1;
 
-		// If the article has a byline, log and append to $articleList
-		var byline = article.byline;
+// 		// Create the  list group to contain the articles and add the article content for each
+// 		var $articleList = $('<ul>');
+// 		$articleList.addClass('list-group');
 
-		if (byline && byline.original) {
-			console.log(byline.original);
-			$articleListItem.append('<h5>' + byline.original + '</h5>');
-		}
+// 		// Add the newly created element to the DOM
+// 		$('#article-section').append($articleList);
 
-		// Log section, and append to document if exists
-		var section = article.section_name;
-		console.log(article.section_name);
-		if (section) {
-			$articleListItem.append('<h5>Section: ' + section + '</h5>');
-		}
+// 		// If the article has a headline, log and append to $articleList
+// 		var headline = article.headline;
+// 		var $articleListItem = $("<li class='list-group-item articleHeadline'>");
 
-		// Log published date, and append to document if exists
-		var pubDate = article.pub_date;
-		console.log(article.pub_date);
-		if (pubDate) {
-			$articleListItem.append('<h5>' + article.pub_date + '</h5>');
-		}
+// 		if (headline && headline.main) {
+// 			console.log(headline.main);
+// 			$articleListItem.append(
+// 				"<span class='label label-primary'>" +
+// 					articleCount +
+// 					'</span>' +
+// 					'<strong> ' +
+// 					headline.main +
+// 					'</strong>'
+// 			);
+// 		}
 
-		// Append and log url
-		$articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + '</a>');
-		console.log(article.web_url);
+// 		// If the article has a byline, log and append to $articleList
+// 		var byline = article.byline;
 
-		// Append the article
-		$articleList.append($articleListItem);
-	}
-}
+// 		if (byline && byline.original) {
+// 			console.log(byline.original);
+// 			$articleListItem.append('<h5>' + byline.original + '</h5>');
+// 		}
 
-// Function to empty out the articles
-function clear() {
-	$('#article-section').empty();
-}
+// 		// Log section, and append to document if exists
+// 		var section = article.section_name;
+// 		console.log(article.section_name);
+// 		if (section) {
+// 			$articleListItem.append('<h5>Section: ' + section + '</h5>');
+// 		}
 
-// CLICK HANDLERS
-// ==========================================================
+// 		// Log published date, and append to document if exists
+// 		var pubDate = article.pub_date;
+// 		console.log(article.pub_date);
+// 		if (pubDate) {
+// 			$articleListItem.append('<h5>' + article.pub_date + '</h5>');
+// 		}
 
-// .on("click") function associated with the Search Button
-$('#run-search').on('click', function(event) {
-	// This line allows us to take advantage of the HTML "submit" property
-	// This way we can hit enter on the keyboard and it registers the search
-	// (in addition to clicks). Prevents the page from reloading on form submit.
-	event.preventDefault();
+// 		// Append and log url
+// 		$articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + '</a>');
+// 		console.log(article.web_url);
 
-	// Empty the region associated with the articles
-	clear();
+// 		// Append the article
+// 		$articleList.append($articleListItem);
+// 	}
+// }
 
-	// Build the query URL for the ajax request to the NYT API
-	var queryURL = buildQueryURL();
+// // Function to empty out the articles
+// function clear() {
+// 	$('#article-section').empty();
+// }
 
-	// Make the AJAX request to the API - GETs the JSON data at the queryURL.
-	// The data then gets passed as an argument to the updatePage function
-	$.ajax({
-		url: queryURL,
-		method: 'GET'
-	}).then(updatePage);
-});
+// // CLICK HANDLERS
+// // ==========================================================
 
-// //  .on("click") function associated with the clear button
-// $('#clear-all').on('click', clear);
+// // .on("click") function associated with the Search Button
+// $('#run-search').on('click', function(event) {
+// 	// This line allows us to take advantage of the HTML "submit" property
+// 	// This way we can hit enter on the keyboard and it registers the search
+// 	// (in addition to clicks). Prevents the page from reloading on form submit.
+// 	event.preventDefault();
+
+// 	// Empty the region associated with the articles
+// 	clear();
+
+// 	// Build the query URL for the ajax request to the NYT API
+// 	var queryURL = buildQueryURL();
+
+// 	// Make the AJAX request to the API - GETs the JSON data at the queryURL.
+// 	// The data then gets passed as an argument to the updatePage function
+// 	$.ajax({
+// 		url: queryURL,
+// 		method: 'GET'
+// 	}).then(updatePage);
+// });
+
+// // //  .on("click") function associated with the clear button
+// // $('#clear-all').on('click', clear);
