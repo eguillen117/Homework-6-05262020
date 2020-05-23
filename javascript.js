472ea572ef76e3551f732560947ea5c287c71efd
Eduardo Guillen <eguillen117@gmail.com>
2020-05-23 12:06:13 -0500

    Attempting to create 5 day forcast function

--- javascript.js
+++ javascript.js
@@ -1,25 +1,20 @@
 //Creating Var
-var apiUvIndex = 
+var apiUvIndex =
+	//Function to get current date and time=============================
+	$(document).ready(function() {
+		//getting the current day and added a new format
+		$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
+		//target tbody to manipulate the table rows ... and more
+		var tbody = $('#calendar-content');
 
-
-
-
-
-//Function to get current date and time=============================
-$(document).ready(function() {
-	//getting the current day and added a new format
-	$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
-	//target tbody to manipulate the table rows ... and more
-	var tbody = $('#calendar-content');
-
-	$('#cityForm').on('submit', function(event) {
-		//Code below prevents the page reloading
-		event.preventDefault();
-		var searchTerm = $('#search-term').val().trim();
-		console.log(searchTerm);
-		gettingJSON(searchTerm);
+		$('#cityForm').on('submit', function(event) {
+			//Code below prevents the page reloading
+			event.preventDefault();
+			var searchTerm = $('#search-term').val().trim();
+			console.log(searchTerm);
+			gettingJSON(searchTerm);
+		});
 	});
-});
 // END OF current day function to get present date=============================================================
 
 function gettingJSON(searchTerm) {
@@ -34,10 +29,37 @@ function gettingJSON(searchTerm) {
 			$('#humidity').text(json.main.humidity);
 			$('#windSpeed').text(json.wind.speed);
 			$('#cityName').text(searchTerm);
-
 		}
 	);
 }
+
+$('#cityForm').on('submit', function(event) {
+	//Code below prevents the page reloading
+	event.preventDefault();
+	var searchTerm = $('#search-term').val().trim();
+	console.log(searchTerm);
+	gettingJSON(searchTerm);
+});
+
+// function gettingJSON(fiveDayForecast) {
+// 	$.getJSON(
+// 		'http://api.openweathermap.org/data/2.5/forecast?q=' +
+// 			searchTerm +
+// 			'&units=imperial&APPID=46e6f2e7796e9e6d3d3f2cc4d3f59ec2',
+// 		function(json) {
+// 			console.log(json);
+// 		}
+// 	);
+// }
+
+// function gettingJSON(searchTerm) {
+// 	$.getJSON(
+// 		'http://api.openweathermap.org/data/2.5/forecast?q=' + searchTerm + '&appid=46e6f2e7796e9e6d3d3f2cc4d3f59ec2',
+// 		function(json) {
+// 			console.log(json);
+// 		}
+// 	);
+// }
 
 //How do I create UV Index API?
 //I need - LAT, LONG, AND then also use this API?
