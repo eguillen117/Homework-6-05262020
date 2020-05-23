1b0373da7410d6ff90bf9b4c71fd4fec10c131fc
Eduardo Guillen <eguillen117@gmail.com>
2020-05-23 17:51:36 -0500

    Updating function on trying to do 5 day forecast

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
-
 // Creating variables to be able store local storage.
 var storedCity = '';
 var searchTerm = [];
 var days = 6;
-
 
 //Function to get current date and time=============================
 $(document).ready(function() {
@@ -29,7 +27,6 @@ function gettingJSON(searchTerm) {
 			'&units=imperial&APPID=46e6f2e7796e9e6d3d3f2cc4d3f59ec2',
 		function(json) {
 			// document.write(JSON.stringify(json));
-			console.log(json);
 			$('#temperature').text(json.main.temp);
 			$('#humidity').text(json.main.humidity);
 			$('#windSpeed').text(json.wind.speed);
@@ -38,21 +35,17 @@ function gettingJSON(searchTerm) {
 	);
 }
 
-// $('#cityForm').on('submit', function(event) {
-// 	//Code below prevents the page reloading
-// 	event.preventDefault();
-// 	var searchTerm = $('#search-term').val().trim();
-// 	console.log(searchTerm);
-// 	gettingJSON(searchTerm);
-// });
+// Start of function to post 5 days
+function fiveDayForecast(searchTerm) {
+	$.getJSON(
+		'https://api.weatherapi.com/v1/forecast.json?key=46e6f2e7796e9e6d3d3f2cc4d3f59ec2&q=' + searchTerm + '&days=6'
+		// // function (json) {
+		// 	var cityName = json.location.name;
+		// 	var cityName = json.location.name;
+		// 	var date = new Date(json.location.localtime).toDateString();
+		// 	var iconUrl = 'https:' + json.forecast.forecastday[0].day.condition.icon;
+		// 	var uv = json.current.uv;
 
-// function gettingJSON(fiveDayForecast) {
-// 	$.getJSON(
-// 		'http://api.openweathermap.org/data/2.5/forecast?q=' +
-// 			searchTerm +
-// 			'&units=imperial&APPID=46e6f2e7796e9e6d3d3f2cc4d3f59ec2',
-// 		function(json) {
-// 			console.log(json);
-// 		}
-// 	);
-// }
+		// }
+	);
+}
