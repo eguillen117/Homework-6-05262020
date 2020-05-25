68716d319ef428ae67e1e07dcb2c9dadc5eaeac6
Eduardo Guillen <eguillen117@gmail.com>
2020-05-25 12:12:51 -0500

    Updated Javascript

--- javascript.js
+++ javascript.js
@@ -36,40 +36,25 @@ $('#cityForm').on('submit', function(event) {
 				$('#humidity').text(json.main.humidity);
 				$('#windSpeed').text(json.wind.speed);
 				$('#cityName').text(searchTerm);
+				// $('#uvIndex').text(json.current.uv);
 			}
 		);
 
-		$.getJSON(
-			'http://api.openweathermap.org/data/forecast.json?key=46e6f2e7796e9e6d3d3f2cc4d3f59ec2&q=' +
-				searchTerm +
-				'&days=1',
-			function(json) {
-				var cityName = json.location.name;
-				var date = new Date(json.location.localtime).toDateString();
-				var uv = json.current.uv;
+		function gettingJSON(searchTerm) {
+			$.getJSON(
+				'http://api.openweathermap.org/data/forecast.json?key=46e6f2e7796e9e6d3d3f2cc4d3f59ec2&q=' +
+					searchTerm +
+					'&days=2',
+				function(json) {
+					var cityName = json.location.name;
+					var date = new Date(json.location.localtime).toDateString();
+					var uv = json.current.uv;
 
-				$('#temperature1').text(json.current.temp_f);
-			}
-		);
+					$('#temperature1').text(json.current.temp_f);
+					$('#humidity1').text(json.current.humidity);
+					console.log(searchTerm);
+				}
+			);
+		}
 	}
 });
-
-// $('#cityForm').on('submit', function(event) {
-// 	//Code below prevents the page reloading
-// 	event.preventDefault();
-// 	var searchTerm = $('#search-term').val().trim();
-// 	console.log(searchTerm);
-// 	gettingJSON(searchTerm);
-// });
-// END OF current day function to get present date=============================================================
-
-// function gettingJSON(searchTerm) {
-// 	$.getJSON(
-// 		'http://api.openweathermap.org/data/2.5/weather?q=' +
-// 			searchTerm +
-// 			'&units=imperial&APPID=46e6f2e7796e9e6d3d3f2cc4d3f59ec2',
-// 		function(json) {
-// 			// document.write(JSON.stringify(json));
-// 			$('#temperature1').text(json.main.temp);
-// 			$('#humidity1').text(json.main.humidity);
-// 		}
